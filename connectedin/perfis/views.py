2a648f6160c601fd6c1127d57acbab5daf701cf7
Vinícius Melo <viniciusdesk@icloud.com>
2018-11-21 14:44:56 -0200

    Modified django project, added extra functions and profiles.

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,6 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from perfis.models import Perfil
 
 #Follow the convention about insert the html page inside the 'templates' folder!!!
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    perfil = Perfil()
+
+    if perfil_id == '1':
+        perfil = Perfil(nome='Flavio Almeida', email='flavio@flavio.com.br', telefone= '34411441', nome_empresa='Alura')
+    elif perfil_id == '2':
+        perfil = Perfil('Romulo Henrique', 'romulo@romulo.com.br', '888888', 'Caelum')
+    elif perfil_id == '3':
+        perfil = Perfil('Vinícius Melo', 'vinicius@vinicius.com', '99999999', 'Open Source')
+
+    print("Id do perfil recebido: %s" % (perfil_id))
+    return render(request, 'perfil.html', {'perfil' : perfil})
