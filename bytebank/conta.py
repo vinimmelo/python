960965809bf70b3dabf240d2c82fce545087d134
Vinícius Melo <viniciusdesk@icloud.com>
2018-11-22 14:44:35 -0200

    Added conta class, project to training object oriented paradigm.

---
+++ bytebank/conta.py
@@ -0,0 +1,12 @@
+# Created on 22 November 2018
+# @author vinimmelo
+# Basic conta class
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
