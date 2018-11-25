a90d319b3cf23e4ac954c1e8f3f73cb7d49438f6
Vinícius Melo <viniciusdesk@icloud.com>
2018-11-25 17:25:47 -0200

    Project 2 - Guess that number game finished.

--- TalkPythonTraining/02_guess_that_number_game/program.py
+++ TalkPythonTraining/02_guess_that_number_game/program.py
@@ -3,11 +3,21 @@ import random
 print('-----------------------------------')
 print('       GUESS THAT NUMBER GAME')
 print('-----------------------------------')
-print("\n\n")
 
 the_number = random.randint(0, 100)
 
-guess_text = input('Guess a number between 0 and 100: ')
-guess = int(guess_text)
+player = input("Player, what's your name? ")
+guess = -50
 
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+    if guess < the_number:
+        print("Sorry {1}, your guess of {0} was too LOW".format(guess, player))
+    elif guess > the_number:
+        print("Sorry {1}, your guess of {0} was too HIGH".format(guess, player))
+    else:
+        print("Excelent work {1}, you won, it was {0}.".format(guess, player))
+
+print('Done!')
 
