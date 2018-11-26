30ec385f9aee79257f16488a37d737a76dac69d9
Vinícius Melo <viniciusdesk@icloud.com>
2018-11-26 14:19:40 -0200

    Added functions and finished the Birthday App.

--- TalkPythonTraining/03_birthday/program.py
+++ TalkPythonTraining/03_birthday/program.py
@@ -1,27 +1,43 @@
-print('-----------------------------------')
-print('       GUESS THAT NUMBER GAME')
-print('-----------------------------------')
-
+import datetime
 
 def print_header():
-    pass
+    print('-----------------------------------')
+    print('           BIRTHDAY APP')
+    print('-----------------------------------')
 
 
 def get_birthday_for_user():
-    pass
+    print('When were you born? ')
+    year = int(input("Year [YYYY]: "))
+    month = int(input("Month [MM]: "))
+    day = int(input("Day [DD]: "))
+
+    birthday = datetime.date(year, month, day)
+    return birthday
 
 
-def compute_days_between_dates():
-    pass
+def compute_days_between_dates(original, target):
+    this_year = datetime.date(target.year, original.month, original.day)
+    dt = this_year - target
+    return dt.days
 
 
-def print_birthday_information():
-    pass
+def print_birthday_information(days):
+    if days < 0:
+        print("You had your birthday {} days ago this year!".format(days))
+    elif days > 0:
+        print("Your birthday is in {} days!".format(days))
+    else:
+        print("Happy Birthday!!!")
 
 
 def main():
     print_header()
-    bday = get_birthday_for_user()
-    now = None
-    number_of_days = compute_days_between_dates()
+    birthday = get_birthday_for_user()
+    today = datetime.date.today()
+    number_of_days = compute_days_between_dates(birthday, today)
     print_birthday_information(number_of_days)
+
+
+if __name__ == '__main__':
+    main()
