d679917363212c540f94e0762a1e11c7d1588d69
Vinícius Melo <viniciusdesk@icloud.com>
2018-11-28 11:02:59 -0200

    Added filme.py file, basic class to training the inheritance and constructors.

---
+++ codes_complete/Alura/filme.py
@@ -0,0 +1,47 @@
+
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def likes(self):
+        return self._likes
+
+
+class Filme (Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+class Serie (Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+if __name__ == '__main__':
+    vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+    atlanta = Serie('atlanta', 2018, 2)
+    vingadores.dar_likes()
+    vingadores.dar_likes()
+    vingadores.dar_likes()
+
+    atlanta.dar_likes()
+    atlanta.dar_likes()
+
+    print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+    print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
