7d6faf3e4fc27116b7d7cc82db4d2b72e99b4ac9
Vinícius Melo <viniciusdesk@icloud.com>
2018-12-06 14:14:34 -0200

    Added journals app, to save your entries and test I/O input and output.

---
+++ TalkPythonTraining/04_journal/program.py
@@ -0,0 +1,50 @@
+import journal
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header() :
+    print('-----------------------------------')
+    print('           BIRTHDAY APP')
+    print('-----------------------------------')
+
+
+def run_event_loop() :
+    print('What you want to do with your journal?')
+
+    cmd = None
+
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)
+
+    while cmd != 'X':
+        cmd = input('[L]ist entries, [A]dd entry, E[x]it: ')
+        cmd = cmd.upper().strip()
+
+        if cmd == 'L':
+            list_entries(journal_data)
+        elif cmd == 'A':
+            add_entries(journal_data)
+        elif cmd != 'X':
+            print('Choose something brow')
+
+    print('Done, goodbye!')
+    journal.save(journal_name, journal_data)
+
+
+def list_entries(data):
+    print('Your entries in the journal:')
+    for ind, entry in enumerate(data):
+        print(f'[{ind + 1}] - {entry.rstrip()}')
+
+
+def add_entries(data):
+    entry = input('Digit your entry to the journal, press <enter> to exit: ')
+    data.append(entry)
+
+
+if __name__ == '__main__':
+    main()
