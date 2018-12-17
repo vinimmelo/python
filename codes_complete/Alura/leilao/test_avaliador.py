530b7e2ccfc577b2ea8da21c933d8ade4866cb14
Vinícius Melo <viniciusdesk@icloud.com>
2018-12-17 11:59:18 -0200

    Improved tests and added two new business rule.

--- codes_complete/Alura/leilao/test_avaliador.py
+++ codes_complete/Alura/leilao/test_avaliador.py
@@ -10,7 +10,7 @@ class TestAvaliador(TestCase) :
         self.lance_do_gui = Lance(self.gui, 150.0)
         self.leilao = Leilao('Celular')
 
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self) :
         yuri = Usuario('Yuri')
         lance_do_yuri = Lance(yuri, 100.0)
 
@@ -22,6 +22,14 @@ class TestAvaliador(TestCase) :
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self) :
+        with self.assertRaises(ValueError) :
+            yuri = Usuario('Yuri')
+            lance_do_yuri = Lance(yuri, 100.0)
+
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_yuri)
 
     def test_deve_retornar_o_mesmo_valor_para_maior_e_menor_lance_quando_leilao_tiver_apenas_um_lance(self):
         self.leilao.propoe(self.lance_do_gui)
@@ -48,3 +56,26 @@ class TestAvaliador(TestCase) :
         self.assertEqual(menor_valor_esperado, leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, leilao.maior_lance)
 
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self) :
+        self.leilao.propoe(self.lance_do_gui)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebido)
+
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self) :
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 200.0)
+
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+
+        self.assertEqual(2, quantidade_de_lances_recebido)
+
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self) :
+        lance_do_gui200 = Lance(self.gui, 200.0)
+
+        with self.assertRaises(ValueError) :
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_gui200)
