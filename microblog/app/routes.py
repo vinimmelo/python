97b80ec3f82c805b09979c3b21a6842a7567fb36
Melo <Vinicius.Melo1@br.nestle.com>
2019-01-03 20:47:13 -0200

    Correct render_template in routes file.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -27,4 +27,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('../../flask_alura/templates/login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form)
