24e17f13225b586ef9c469511b08feb260f31858
Vinícius Melo <viniciusdesk@icloud.com>
2019-01-13 01:32:19 -0200

    Weather App added to TalkPythonTraining, simple app that retrieve the weather in the determined region, by the Zip Code.

---
+++ TalkPythonTraining/05_weather_client/program.py
@@ -0,0 +1,15 @@
+
+def main():
+    print_the_header()
+    code = input('What zipcode do you want the weather for (97201)? ')
+    print(code)
+
+
+def print_the_header():
+    print('-----------------------------------')
+    print('           WEATHER APP')
+    print('-----------------------------------')
+
+
+if __name__ == '__main__':
+    main()
