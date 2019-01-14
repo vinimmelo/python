2d1c76cfc7b645617f378fd8c1bd564a40748d9a
Vinícius Melo <viniciusdesk@icloud.com>
2019-01-14 14:19:52 -0200

    Added get html from web function in weather app.

--- TalkPythonTraining/05_weather_client/program.py
+++ TalkPythonTraining/05_weather_client/program.py
@@ -2,13 +2,19 @@
 def main():
     print_the_header()
     code = input('What zipcode do you want the weather for (97201)? ')
-    print(code)
+    url = get_html_from_web('us/or/portland')
+    print(url)
 
 
 def print_the_header():
     print('-----------------------------------')
     print('           WEATHER APP')
     print('-----------------------------------')
+
+
+def get_html_from_web(city):
+    url = 'https://www.wunderground.com/weather/{}'.format(city)
+    return url
 
 
 if __name__ == '__main__':
