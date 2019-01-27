e69df54f139ebbbbfb95390e5bf78d75883fff4c
Vinícius Melo <viniciusdesk@icloud.com>
2019-01-27 12:22:31 -0200

    Added module and continued the program, initiate beautifulsoup4 parser.

--- TalkPythonTraining/05_weather_client/program.py
+++ TalkPythonTraining/05_weather_client/program.py
@@ -1,9 +1,14 @@
+import requests
+import bs4
+
+
 
 def main():
     print_the_header()
-    code = input('What zipcode do you want the weather for (97201)? ')
-    url = get_html_from_web('us/or/portland')
-    print(url)
+    html_page = get_html_from_web('us/or/portland')
+    get_weather_from_html(html_page)
+    # parse html
+    # display the selected data
 
 
 def print_the_header():
@@ -14,7 +19,19 @@ def print_the_header():
 
 def get_html_from_web(city):
     url = 'https://www.wunderground.com/weather/{}'.format(city)
-    return url
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    return ''
+
+
+def get_weather_from_html(html_page):
+    cityCss = '.region-content-header-h1'
+    weatherScaleCss = '.wu-unit-temperature.wu-label'
+    weatherTempCss = '.wu-unit-temperature.wu-label'
+    weatherConditionCss = '.condition-icon'
+    soup = bs4.BeautifulSoup(html_page, 'html.parser')
+    pass
 
 
 if __name__ == '__main__':
