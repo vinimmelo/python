8257ac01c5641c2f3e77988d2a334e5d77fe2264
Melo <Vinicius.Melo1@br.nestle.com>
2019-02-08 22:03:52 -0200

    Refactor of strategy pattern

--- CodesComplete/FluentPython/strategy.py
+++ CodesComplete/FluentPython/strategy.py
@@ -60,3 +60,12 @@ def large_order_promo(order):
     if len(distinct_items) >= 10:
         return order.total() * .07
     return 0
+
+
+promos = [globals()[name] for name in globals()
+          if name.endswith('_promo')
+          and name != 'best_promo']
+
+def best_promo(order):
+    """Select the best discount available"""
+    return max(promo(order) for promo in promos)
