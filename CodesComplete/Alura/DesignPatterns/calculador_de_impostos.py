8c5e554de9a182eb25d175ee481f5f7d69a8fa63
Vinícius Melo <viniciusdesk@icloud.com>
2019-02-09 19:02:12 -0200

    Template pattern implementation...

--- CodesComplete/Alura/DesignPatterns/calculador_de_impostos.py
+++ CodesComplete/Alura/DesignPatterns/calculador_de_impostos.py
@@ -1,16 +1,26 @@
-from impostos import calcula_icms, calcula_iss
 
 class Calculador_de_impostos:
 
     def realiza_calculo(self, orcamento, imposto):
-        imposto_calculado = imposto(orcamento)
+        imposto_calculado = imposto.calcula(orcamento)
         print(imposto_calculado)
 
 
 if __name__ == '__main__':
-    from orcamento import Orcamento
+    from orcamento import Orcamento, Item
 
-    orcamento = Orcamento(500.0)
+    from impostos import ISS, ICMS, ICPP, IKCV
+
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('ITEM 1', 50))
+    orcamento.adiciona_item(Item('ITEM 2', 200))
+    orcamento.adiciona_item(Item('ITEM 3', 250))
+
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento, calcula_icms)
-    calculador_de_impostos.realiza_calculo(orcamento, calcula_iss)
+    print('ISS e ICMS')
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
+
+    print('ICPP e IKCV')
+    calculador_de_impostos.realiza_calculo(orcamento, ICPP())
+    calculador_de_impostos.realiza_calculo(orcamento, IKCV())
--- CodesComplete/Alura/DesignPatterns/impostos.py
+++ CodesComplete/Alura/DesignPatterns/impostos.py
@@ -1,9 +1,70 @@
-"""Strategy Pattern"""
+# -*- coding: UTF-8 -*-
 
-def calcula_icms(orcamento):
-    return orcamento.valor * 0.1
+from abc import ABCMeta, abstractmethod
 
 
-def calcula_iss(orcamento):
-    return orcamento.valor * 0.06
+class TemplateDeImpostoCondicional:
+    __metaclass__ = ABCMeta
 
+    def calcula(self, orcamento):
+
+        if self.deve_usar_maxima_taxacao(orcamento):
+            return self.maxima_taxacao(orcamento)
+        else:
+            return self.minima_taxacao(orcamento)
+
+    @abstractmethod
+    def deve_usar_maxima_taxacao(self, orcamento):
+        pass
+
+    @abstractmethod
+    def maxima_taxacao(self, orcamento):
+        pass
+
+    @abstractmethod
+    def minima_taxacao(self, orcamento):
+        pass
+
+
+class ISS:
+
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.1
+
+
+class ICMS:
+
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.06
+
+
+class ICPP(TemplateDeImpostoCondicional):
+
+    def deve_usar_maxima_taxacao(self, orcamento):
+        return orcamento.valor > 500
+
+    def maxima_taxacao(self, orcamento):
+        return orcamento.valor * 0.07
+
+    def minima_taxacao(self, orcamento):
+        return orcamento.valor * 0.05
+
+
+class IKCV(TemplateDeImpostoCondicional):
+
+    def deve_usar_maxima_taxacao(self, orcamento):
+
+        return orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento)
+
+    def maxima_taxacao(self, orcamento):
+        return orcamento.valor * 0.1
+
+    def minima_taxacao(self, orcamento):
+        return orcamento.valor * 0.06
+
+    def __tem_item_maior_que_100_reais(self, orcamento):
+
+        for item in orcamento.obter_itens():
+            if item.valor > 100:
+                return True
+        return False
