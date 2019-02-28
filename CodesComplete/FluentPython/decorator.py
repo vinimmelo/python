6686fe0fa8661d6f70f23a4cf060db4aead36367
Melo <Vinicius.Melo1@br.nestle.com>
2019-02-27 22:35:53 -0300

    Generator and decorator modules created.

---
+++ CodesComplete/FluentPython/decorator.py
@@ -0,0 +1,45 @@
+"""
+Decorator training
+"""
+
+def deco(func):
+    def inner():
+        print('running inner()')
+    return inner
+
+
+@deco
+def target():
+    print('running target()')
+
+
+registry = []
+
+def register(func):
+    print(f'running register {func}')
+    registry.append(func)
+    return func
+
+
+@register
+def f1():
+    print('running f1()')
+
+@register
+def f2():
+    print('running f2()')
+
+def f3():
+    print('running f3()')
+
+
+def main():
+    print('running main()')
+    print('registry ->', registry)
+    f1()
+    f2()
+    f3()
+
+
+if __name__ == '__main__':
+    main()
